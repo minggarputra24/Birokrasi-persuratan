867daf40b735ecccdf96caf6a9ee3a3041f0b6f8
raihanrach <raihanrach@gmail.com>
2021-06-27 15:00:25 +0700

    updated paling fix

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -10,7 +10,7 @@
                     <!-- Nested Row within Card Body -->
                     <div class="row">
                         <!-- Ini Bagian Gambar pada login page -->
-                        <div class="col-lg-6 d-none d-lg-block "></div>
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
