a49237a10ea91c4405a64e054561707c2d08cd34
raihanrach <raihanrach@gmail.com>
2021-06-30 11:02:44 +0700

    v #5 done
    -error akun tidak terdaftar ttp bisa masuk

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,17 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'My Profil';
-        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-
+        $data['title'] = 'Bereanda';
+        $data['user'] = $this->db->get_where('user', ['username' =>
+        $this->session->userdata('username')])->row_array();
+        
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
 }
