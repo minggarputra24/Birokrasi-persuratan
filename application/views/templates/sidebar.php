17c2bcfc308258f2022a026f22d1d76af98a503b
andhikautama <andhikautamaa04@gmail.com>
2021-07-01 10:05:33 +0700

    v6 done

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -10,21 +10,55 @@
 </a>
 
 <!-- Divider -->
-<hr class="sidebar-divider my-0">
-
-<!-- Nav Item - Beranda -->
-<li class="nav-item">
-    <a class="nav-link" href="index.html">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Beranda</span></a>
-</li>
-
-<!-- Divider -->
 <hr class="sidebar-divider">
+
+<!-- QUERY MENU -->
+<?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                  FROM `user_menu` JOIN `user_access_menu` 
+                  ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                  WHERE `user_access_menu`.`role_id` = $role_id
+                  ORDER BY `user_access_menu`.`menu_id` ASC
+                  ";
+
+$menu = $this->db->query($queryMenu)->result_array();
+?>
+
+<!-- LOOPING MENU -->
+<?php foreach($menu as $m) : ?>
+<div class="sidebar-heading">
+    <?= $m['menu']; ?>
+</div>
+
+<!-- SIAPKAN SUB-MENU SESUAI MENU -->
+<?php
+    $menuId = $m['id'];
+    $querySubMenu = "SELECT *
+                     FROM `user_sub_menu` JOIN `user_menu` 
+                     ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                     WHERE `user_sub_menu`.`menu_id` = $menuId
+                     AND `user_sub_menu`.`is_active` = 1
+                     ";
+
+$SubMenu = $this->db->query($querySubMenu)->result_array();
+?>
+
+<?php foreach($SubMenu as $sm) : ?>
+    <li class="nav-item">
+    <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+    <i class="<?= $sm['icon']; ?>"></i>
+        <span><?= $sm['title'] ?></span></a>
+</li>
+<?php endforeach ; ?>
+
+<hr class="sidebar-divider">
+
+<?php endforeach; ?>
 
 <!-- Heading -->
 <div class="sidebar-heading">
-    Interface
+    Persuratan
 </div>
 
 <!-- Nav Item - Pages Collapse Menu -->
@@ -53,34 +87,20 @@
     </div>
 </li>
 
-<!-- Nav Item - Utilities Collapse Menu -->
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-        aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>Pengaturan</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Menu:</h6>
-            <a class="collapse-item" href="utilities-color.html">
-                <i class="fas fa-user-tie"></i>
-                <span>User</span>
-            </a>
-            <!-- <a class="collapse-item" href="utilities-border.html">Borders</a> -->
-            <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a> -->
-            <!-- <a class="collapse-item" href="utilities-other.html">Other</a> -->
-        </div>
-    </div>
-</li>
+<!-- <hr class="sidebar-divider">
+<div class="sidebar-heading">
+    User
+</div>
 
-<!-- Divider -->
-<hr class="sidebar-divider">
+<li class="nav-item">
+    <a class="nav-link" href="index.html">
+    <i class="fas fa-user-tie"></i>
+        <span>User</span></a>
+</li> -->
 
 <!-- Heading -->
 <div class="sidebar-heading">
-    Addons
+    Keluar
 </div>
 
 <!-- Nav Item - Pages Collapse Menu -->
@@ -106,21 +126,21 @@
 </li> -->
 
 <!-- Nav Item - Charts -->
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a class="nav-link" href="charts.html">
         <i class="fas fa-fw fa-chart-area"></i>
         <span>Charts</span></a>
-</li>
+</li> -->
 
 <!-- Nav Item - Tables -->
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a class="nav-link" href="tables.html">
         <i class="fas fa-fw fa-table"></i>
         <span>Tables</span></a>
-</li>
+</li> -->
 
 <!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
+<!-- <hr class="sidebar-divider d-none d-md-block"> -->
 
 <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
