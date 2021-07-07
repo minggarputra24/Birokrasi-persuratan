740169cc517ebd4ee9a0167b74e10763df8aa417
andhikautama <andhikautamaa04@gmail.com>
2021-07-07 14:44:39 +0700

    update suratmasuk

--- application/views/user/suratMasuk.php
+++ application/views/user/suratMasuk.php
@@ -10,6 +10,33 @@
         </div>
     </div> -->
 
+    <hr class="border-dark border-5 mt-1">
+    <a href="#" class="btn btn-primary mb-3">
+    <i class="fas fa-envelope-open-text"></i>
+    Tambah Surat Masuk Baru
+    </a>
+    
+    <div class="input-group rounded mb-3">
+        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
+        aria-describedby="search-addon" />
+            <span class="input-group-text border-5" id="search-addon">
+                <i class="fas fa-search"></i>
+            </span>
+    </div>
+
+    <!-- <div class="dropdown show mb-3">
+        <span>Show : </span>
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button"
+        id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            
+        </a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" option value="10">10</a>
+            <a class="dropdown-item" option value="50">50</a>
+            <a class="dropdown-item" href="#">100</a>
+            </div>
+    </div> -->
+
     <table class="table table-bordered table-striped">
         <thead class="table-dark">
         <tr>
@@ -33,9 +60,11 @@
                 <td class="text-center"> contoh</td>
                 <td class="text-center"> contoh</td>
                 <td class="text-center">
-                    <a  class="btn btn-sm btn-success mb-3">Update</a>
-                    <a  class="btn btn-sm btn-danger mb-3">Delete</a>
-                    <a  class="btn btn-sm btn-warning mb-3">Print</a>
+                    <a  class="btn btn-sm btn-success mb-2">Update</a>
+                    <a  class="btn btn-sm btn-danger mb-2">Delete</a>
+                    <br>
+                    <a  class="btn btn-sm btn-warning mb-1">Print</a>
+                    <a  class="btn btn-sm btn-info mb-1">Disposisi</a>
                 </td>
             </tr>
         
